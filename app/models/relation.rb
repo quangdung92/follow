96d92163cd2f6a51b11d9682ca928c9f7c72ec24
quangdung92 <quangdung.hvbc@gmail.com>
2014-10-21 11:30:16 +0700

    Create relation.rb

---
+++ app/models/relation.rb
@@ -0,0 +1,5 @@
+class Relation < ActiveRecord::Base
+  attr_accessible :follower_id, :following_id
+  belongs_to :follower, :class_name => "User"
+  belongs_to :following, :class_name => "User"
+end
