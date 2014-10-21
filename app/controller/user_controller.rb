fc42d57aa33969899c64f947d6fdbf8581b14ab8
quangdung92 <quangdung.hvbc@gmail.com>
2014-10-21 11:26:34 +0700

    Create user_controller.rb

---
+++ app/controller/user_controller.rb
@@ -0,0 +1,13 @@
+class UserController < ApplicationController
+  def new
+    @user = User.new
+  end
+  def signup
+    @user = User.new(:username => params[:username], :pass => params[:pass])
+    if @user.save
+      redirect_to welcome_index_path
+    else
+      render "new"
+    end
+  end
+end
