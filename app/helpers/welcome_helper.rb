7200a724bb25bbf792843089bec1f179602017f4
quangdung92 <quangdung.hvbc@gmail.com>
2014-10-22 17:36:17 +0700

    Update welcome_helper.rb

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,2 +1,8 @@
 module WelcomeHelper
+  def login(user)
+    session[:user_id] = user.id
+  end
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
 end
