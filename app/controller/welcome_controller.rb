99a3665e424299c28a942803b4ab67ed01969421
quangdung92 <quangdung.hvbc@gmail.com>
2014-10-23 21:31:58 +0700

    Update welcome_controller.rb

--- app/controller/welcome_controller.rb
+++ app/controller/welcome_controller.rb
@@ -1,18 +1,18 @@
 class WelcomeController < ApplicationController
   def index
+     @users = User.all
   end
  def create
     user = User.login(params[:session][:username], params[:session][:pass])
       if user
       login user
-      redirect_to user
+      redirect_to root_url 
     else
       render 'index'
     end
     end
  def sucess
    @user = User.find(params[:id])
-   @users = User.all
    @relation = Relation.new
  end
 end
