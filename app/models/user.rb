d2df5b462e27759be3311020797f05cd306728ce
quangdung92 <quangdung.hvbc@gmail.com>
2014-10-23 21:33:17 +0700

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
 class User < ActiveRecord::Base
+  
   attr_accessible :pass, :username
-  has_many :relations, :foreign_key => :follower_id
-  has_many :following, :through => :relations
-  has_many :relations, :class_name => "Relation", :foreign_key => :following_id
-  has_many :followers, :through => :relations
-
+  # Who am I following?
+  has_many :following_relations, :class_name => "Relation", :foreign_key => :follower_id
+  has_many :following, :through => :following_relations 
+  # Who am I followed by?
+  has_many :follower_relations, :class_name => "Relation", :foreign_key => :following_id
+  has_many :followers, :through => :follower_relations
   
   validates :username, :pass, :presence => true
   validates :username, :pass, :length => { :minimum => 4 }
@@ -20,4 +22,3 @@ class User < ActiveRecord::Base
     end
     end
 end
-
