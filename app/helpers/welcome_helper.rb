5e3ef017c03994d506b572853234ad3b49c4e9ba
quangdung92 <quangdung.hvbc@gmail.com>
2014-11-11 14:05:56 +0700

    Update welcome_helper.rb

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -3,6 +3,9 @@ module WelcomeHelper
     session[:user_id] = user.id
   end
   def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    current_user = User.find(session[:user_id]) if session[:user_id]
+  end
+  def logout
+    session[:user_id] = nil
   end
 end
