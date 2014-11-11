952959ecda5136827c40336b16624df6ac5800e9
quangdung92 <quangdung.hvbc@gmail.com>
2014-11-11 14:01:44 +0700

    Update user_controller.rb

--- app/controller/user_controller.rb
+++ app/controller/user_controller.rb
@@ -3,7 +3,8 @@ class UserController < ApplicationController
     @user = User.new
   end
   def signup
-    @user = User.new(:username => params[:username], :pass => params[:pass])
+    @user = User.new(:username => params[:username], :pass => params[:pass], 
+                     :age => params[:age], :image => params[:image])
     if @user.save
       redirect_to welcome_index_path
     else
@@ -11,4 +12,5 @@ class UserController < ApplicationController
     end
   end
 end
+
 
