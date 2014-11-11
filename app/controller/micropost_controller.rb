9ef272d037ea064a657a758b8da2a37534a41015
quangdung92 <quangdung.hvbc@gmail.com>
2014-11-11 14:02:25 +0700

    Update micropost_controller.rb

--- app/controller/micropost_controller.rb
+++ app/controller/micropost_controller.rb
@@ -3,12 +3,25 @@ class MicropostController < ApplicationController
     @post = Micropost.new
     @posts = current_user.microposts.all
   end
+
   def create
     @post = current_user.microposts.create(:content => params[:content])
+    logger.debug "New post: #{@post.attributes.inspect}"
+    logger.debug "Post should be valid: #{@post.valid?}"
     if @post
-    redirect_to micropost_new_path
+      redirect_to micropost_new_path
     else
+    end
   end
+
+  def edit
+    @micropost = Micropost.find(params[:id])
+    logger.debug "Post should be valid: #{@micropost.id}"
+    logger.debug "Post should be valid: #{@micropost.content}"
+    if @micropost.update_attributes(params[:content])
+    logger.debug "new content: #{@micropost.content?}"
+    else
+      reder 'edit'
+    end
   end
 end
-
