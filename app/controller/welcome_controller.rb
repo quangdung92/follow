66a4f5fa0e187468053786b0eac2c01e1494a173
quangdung92 <quangdung.hvbc@gmail.com>
2014-11-11 14:01:01 +0700

    Update welcome_controller.rb

--- app/controller/welcome_controller.rb
+++ app/controller/welcome_controller.rb
@@ -1,18 +1,31 @@
 class WelcomeController < ApplicationController
   def index
-     @users = User.all
+    @users = (current_user.blank? ? User.all : User.find(:all, :conditions => ["id != ?", current_user.id]))
   end
- def create
+
+  def create
     user = User.login(params[:session][:username], params[:session][:pass])
-      if user
+    if user
       login user
-      redirect_to root_url 
+      redirect_to root_url
     else
-      render 'index'
+      redirect_to welcome_index_path
     end
-    end
- def sucess
-   @user = User.find(params[:id])
-   @relation = Relation.new
- end
+  end
+
+  def sucess
+    @user = User.find(params[:id])
+    @relation = Relation.new
+  end
+
+  def edit
+  @user = User.find(params[:id])
+  @user.assign_attributes(:image => params[:image])
+  @user.save if @user.changed?
+end
+
+  def destroy
+    logout
+    redirect_to root_url
+  end
 end
